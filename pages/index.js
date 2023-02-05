e76a6518dd1d47ae812c8c7ade26dd1ed360aefe
T.Yajima <tatsurou.yajima@gmail.com>
2023-02-05 21:06:57 +0900

    :sparkles: Read this page!まで実装

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
+}
