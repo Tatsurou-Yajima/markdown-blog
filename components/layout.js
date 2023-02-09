a51a97e747a1a382ceb6947eb5cd9f423cccef68
T.Yajima <tatsurou.yajima@gmail.com>
2023-02-09 10:41:56 +0900

    :bug: 画像が表示されない件を修正

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script'
 
-const name = "Your Name";
-export const siteTitle = "Next.js Sample Website";
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = '[Your Name]';
+export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
     return (
@@ -14,17 +16,24 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js" />
+                        content="Learn how to build a personal website using Next.js"
+                    />
                 <meta
                     property="og:image"
-                    content="{`https://og-image.vercel.app/${encodeURL(
-                    siteTitle,
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`
-                }"/>
-                <meta name="og:title" content="siteTitle" />
-                <meta name="twitter:card" content="summary_large_image" />
-                {children}
+                        content={`https://og-image.vercel.app/${encodeURI(
+                            siteTitle
+                        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    />
+                    <meta name="og:title" content={siteTitle} />
+                    <meta name="twitter:card" content="summary_large_image" />
             </Head>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
             <header className={styles.header}>
                 {home ? (
                     <>
@@ -34,7 +43,7 @@ export default function Layout({ children, home }) {
                             className={utilStyles.borderCircle}
                             height={144}
                             width={144}
-                            alt=""
+                                alt={name}
                         />
                         <h1 className={utilStyles.heading2Xl}>{name}</h1>
                     </>
@@ -47,7 +56,7 @@ export default function Layout({ children, home }) {
                                 className={utilStyles.borderCircle}
                                 height={108}
                                 width={108}
-                                alt=""
+                                        alt={name}
                             />
                         </Link>
                         <h2 className={utilStyles.headingLg}>
@@ -66,4 +75,4 @@ export default function Layout({ children, home }) {
             )}
         </div>
     );
-}
+    }
