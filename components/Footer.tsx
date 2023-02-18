2921fa2c2920df254203799f90c4de4d6bc69d17
T.Yajima <tatsurou.yajima@gmail.com>
2023-02-18 11:54:02 +0900

    :bug: PC表示の時も下部にプロフィールが表示されていたので調整した

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/layout.module.css'
 import Profile from './Profile';
 import FooterLogo from './FooterLogo'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Footer() {
     return (
         <div className={`${styles.contentInner}`}>
-            <div className={`${styles.container} ${styles.footer} ${styles.boxShadow}`}>
+            <div className={`${utilStyles.disabledInPC} ${styles.container} ${styles.footer} ${styles.boxShadow}`}>
                 <div className={styles.footer}>
                     <Profile />
                 </div>
