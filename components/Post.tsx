0b0627cd1a6ff92b2914ac891826e205b8e6bc46
T.Yajima <tatsurou.yajima@gmail.com>
2023-05-26 18:41:55 +0900

    🚜 [id]が複雑になっていたので、コンポーネントを分割した

---
+++ components/Post.tsx
@@ -0,0 +1,47 @@
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CodeBlock from './CodeBlock';
+import Date from './date';
+import Head from 'next/head';
+import Layout from './layout';
+import ReactMarkDown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import UtilStyles from '../styles/utils.module.css';
+
+const PostComponent = ({
+    postData
+}: {
+    postData: {
+        title: string
+        date: string
+        contentHtml: string
+        thumbnail: string
+    }
+}) => {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+                <meta name='og:title' content={postData.title} />
+                <meta name="twitter:title" content={postData.title} />
+                <meta name="twitter:title" content={postData.title} />
+                <meta name='twitter:image' content={postData.thumbnail} />
+                <meta property='og:image' content={postData.thumbnail} />
+            </Head>
+            <article className={UtilStyles.articlePadding}>
+                <div className={UtilStyles.lightText}>
+                    <FontAwesomeIcon className={UtilStyles.calenderIcon} icon={faCalendarDays} />
+                    <Date dateString={postData.date} />
+                </div>
+                <ReactMarkDown
+                    components={CodeBlock}
+                    remarkPlugins={[remarkGfm]}
+                >
+                    {postData.contentHtml}
+                </ReactMarkDown>
+            </article>
+        </Layout>
+    );
+};
+
+export default PostComponent;
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,49 +1,9 @@
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Date from '../../components/date';
-import Head from 'next/head';
-import Layout from '../../components/layout';
-import ReactMarkDown from 'react-markdown';
-import UtilStyles from '../../styles/utils.module.css';
-import CodeBlock from '../../components/CodeBlock';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
-import remarkGfm from 'remark-gfm';
+import Post from '../../components/Post'
 
-const Post = ({
-    postData
-}: {
-    postData: {
-        title: string
-        date: string
-        contentHtml: string
-        thumbnail: string
-    }
-    }) => {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-                <meta name='og:title' content={postData.title} />
-                <meta name="twitter:title" content={postData.title} />
-                <meta name="twitter:title" content={postData.title} />
-                <meta name='twitter:image' content={postData.thumbnail} />
-                <meta property='og:image' content={postData.thumbnail} />
-            </Head>
-            <article className={UtilStyles.articlePadding}>
-                <div className={UtilStyles.lightText}>
-                    <FontAwesomeIcon className={UtilStyles.calenderIcon} icon={faCalendarDays} />
-                    <Date dateString={postData.date} />
-                </div>
-                <ReactMarkDown
-                    components={CodeBlock}
-                    remarkPlugins={[remarkGfm]}
-                >
-                    {postData.contentHtml}
-                </ReactMarkDown>
-            </article>
-        </Layout>
-    );
+const PostPage = ({ postData }) => {
+    return <Post postData={postData} />
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -63,4 +23,4 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     };
 }
 
-export default Post;
+export default PostPage;
