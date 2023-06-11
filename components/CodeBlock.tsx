814b1916c5deb83808b544d70bc251dafd1841bc
T.Yajima <tatsurou.yajima@gmail.com>
2023-06-11 11:30:04 +0900

    :tada: コピーボタンを設置

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { CodeComponent } from 'react-markdown/lib/ast-to-react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { FaRegClone } from "react-icons/fa";
 
 const customCode: CodeComponent = ({ node, inline, className, children, ...props }) => {
+    const [isCopied, setIsCopied] = React.useState(false);
+    const onCopyClicked = () => {
+        setIsCopied(true);
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 4000);
+    };
     const match = /language-(\w+)(:?.*)/.exec(className || '');
     const language = match && match[1] ? match[1] : '';
     const fileName = match && match[2] ? match[2].slice(1) : '';
@@ -14,29 +23,53 @@ const customCode: CodeComponent = ({ node, inline, className, children, ...props
         <div>
             <div className="code-block-wrapper">
                 {fileName && <div className="code-block-title">{fileName}</div>}
-                <SyntaxHighlighter
-                    style={atomDark}
-                    language={language}
-                    PreTag="div" {...props}
-                    className={syntaxHighlighterClass}
-                >
-                    {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
+                <span className='code-block-body'>
+                    <SyntaxHighlighter
+                        style={atomDark}
+                        language={language}
+                        PreTag="div" {...props}
+                        className={syntaxHighlighterClass}
+                    >
+                        {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                    <CopyToClipboard text={children}>
+                        <button onClick={onCopyClicked} className="copy-button">
+                            <span>{isCopied ? "copied!" : <FaRegClone />}</span>
+                        </button>
+                    </CopyToClipboard>
+                </span>
             </div>
             <style jsx>{`
             .code-block-wrapper {
-            font-size: 0.9rem;
-            margin-bottom: 2rem;
+                font-size: 0.9rem;
+                margin-bottom: 2rem;
+            }
+            .code-block-body {
+                position: relative;
+                height: 100%;
+                display: block;
             }
             .code-block-title {
-            display: inline-block;
-            border-radius: 0.3rem 0.3rem 0 0;
-            background-color: #323e52;
-            padding: 0.55rem 1rem;
-            color: white;
-            font-size: 0.8rem;
-            font-family: Inconsolata, Monaco, Consolas, "Courier New", Courier,
-                monospace;
+                display: inline-block;
+                border-radius: 0.3rem 0.3rem 0 0;
+                background-color: #323e52;
+                padding: 0.55rem 1rem;
+                color: white;
+                font-size: 0.8rem;
+                font-family: Inconsolata, Monaco, Consolas, "Courier New", Courier,
+                    monospace;
+                position: relative;
+            }
+            .copy-button {
+                position: absolute;
+                right: 0;
+                top: 0px;
+                border: 0px;
+                background: transparent;
+                color: #f2f2f2;
+                padding: 12px;
+                font-size: 1em;
+                cursor: pointer;
             }
         `}</style>
             <style jsx global>{`
@@ -45,9 +78,9 @@ const customCode: CodeComponent = ({ node, inline, className, children, ...props
                 padding: 1.5rem !important;
             }
             .code-block-with-title {
-            border-radius: 0 0.3rem 0.3rem 0.3rem !important;
-            padding: 1.5rem !important;
-            margin-top: 0 !important;
+                border-radius: 0 0.3rem 0.3rem 0.3rem !important;
+                padding: 1.5rem !important;
+                margin-top: 0 !important;
             }
         `}</style>
         </div>
