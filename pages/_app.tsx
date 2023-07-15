eb410b94e8e185b3b65ad0b5678de00ba051f572
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-15 18:23:10 +0900

    ✨ next-seoを追加。Headコンポーネントを修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,41 @@
 import { AppProps } from "next/app"
+import { DefaultSeo } from 'next-seo'
+import Head from "next/head"
 import "../styles/global.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                name='viewport'
+                content='minimum-scale=1, initial-scale=1, width=device-width'
+            </Head>
+            <DefaultSeo
+                defaultTitle='YajiMaga-TECH'
+                description='サーバーサイドエンジニアの技術ブログです。バグ解消法や最近学んだことなどを発信していきます。'
+                openGraph={{
+                    type: 'website',
+                    description: 'サーバーサイドエンジニアの技術ブログです。バグ解消法や最近学んだことなどを発信していきます。',
+                    site_name: 'YajiMaga-TECH',
+                    url: 'https://yajimaga-tech-blog.vercel.app/',
+                    images: [
+                        {
+                            url: 'https://images-for-yajima-tech-blog.s3.ap-northeast-1.amazonaws.com/top-image-20230214.jpg',
+                            width: 800,
+                            height: 600,
+                            alt: 'YajiMaga-TECH',
+                            type: 'image/jpeg',
+                        },
+                    ],
+                }}
+                twitter={{
+                    handle: '@Yazmatto',
+                    site: '@Yazmatto',
+                    cardType: 'summary_large_image',
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }
