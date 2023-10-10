8fdb34f81ccf58e2aa0722237d9361b1e5db999d
T.Yajima <tatsurou.yajima@gmail.com>
2023-10-10 14:24:03 +0900

    🎉 sitemapの自動生成コマンドを記述

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://yajimaga-tech-blog.vercel.app',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
 }
