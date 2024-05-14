5be974ed2f3b46213391f8df49061752a929e9f1
T.Yajima <tatsurou.yajima@gmail.com>
2024-05-14 09:01:34 +0900

    🔧 update domains

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://yajimaga-tech-blog.vercel.app',
+    siteUrl: process.env.SITE_URL || 'https://yajima.sytes.net',
     generateRobotsTxt: true,
     sitemapSize: 7000,
 }
